c0b54065d3bd85086e4dda3d47e3e4c9c3da38e7
Glenn Butcher <glenn.butcher@gmail.com>
2020-02-18 16:11:20 -0700

    first fully working version

--- curlfoo.cpp
+++ curlfoo.cpp
@@ -2,8 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
+#include <unistd.h>
+#include <sys/param.h>
+
 #include <curl/curl.h>
-//#include "json.hpp"
+#include <archive.h>
+#include <archive_entry.h>
+
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -14,7 +19,12 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <sstream>
 
+
+//utility routines:
 
 std::vector<std::string> filelist()
 {
@@ -34,6 +44,13 @@ std::vector<std::string> filelist()
                 closedir(dir);
         }
 	return files;
+}
+
+//https://stackoverflow.com/questions/2203159/is-there-a-c-equivalent-to-getcwd
+std::string get_cwd()
+{
+   char temp[MAXPATHLEN];
+   return ( getcwd(temp, sizeof(temp)) ? std::string( temp ) : std::string("") );
 }
 
 std::string string_format(const std::string fmt, ...) 
@@ -101,16 +118,8 @@ std::string removeall(std::string str, char c)
 	return out;
 }
 
-//std::vector<std::string> parseLensfunVersions(std::string json)
-//{
-//	json.
-//
-//}
 
-
-
-
-
+//curl routines:
 
 struct MemoryStruct {
   char *memory;
@@ -175,7 +184,6 @@ std::string getAsString(std::string url)
 
 }
 
-
 bool getAndSaveFile(std::string url)
 {
 	bool result = true;
@@ -209,17 +217,104 @@ bool getAndSaveFile(std::string url)
 	return result;
 }
 
+//libarchive routines:
+
+static int copy_data(struct archive *ar, struct archive *aw)
+{
+        int r;
+        const void *buff;
+        size_t size;
+#if ARCHIVE_VERSION_NUMBER >= 3000000
+        int64_t offset;
+#else
+        off_t offset;
+#endif
+
+        for (;;) {
+                r = archive_read_data_block(ar, &buff, &size, &offset);
+                if (r == ARCHIVE_EOF)
+                        return (ARCHIVE_OK);
+                if (r != ARCHIVE_OK)
+                        return (r);
+                r = archive_write_data_block(aw, buff, size, offset);
+                if (r != ARCHIVE_OK) {
+                        return (r);
+                }
+        }
+}
+
+static int extract(const char *filename, int flags)
+{
+        struct archive *a;
+        struct archive *ext;
+        struct archive_entry *entry;
+        int r;
+
+        a = archive_read_new();
+        ext = archive_write_disk_new();
+        archive_write_disk_set_options(ext, flags);
+
+        archive_read_support_format_all(a);
+        archive_read_support_filter_all(a);
+
+        if (filename != NULL && strcmp(filename, "-") == 0)
+                filename = NULL;
+        if ((r = archive_read_open_filename(a, filename, 10240))) {
+                printf("%s\n",archive_error_string(a));
+                return -1; //fail("archive_read_open_filename()",archive_error_string(a), r);
+        }
+
+        for (;;) {
+                r = archive_read_next_header(a, &entry);
+                if (r == ARCHIVE_EOF)
+                        break;
+
+                if (r != ARCHIVE_OK)
+                        return -2; //fail("archive_read_next_header()",archive_error_string(a), 1);
+
+                r = archive_write_header(ext, entry);
+                copy_data(a, ext);
+                r = archive_write_finish_entry(ext);
+                if (r != ARCHIVE_OK)
+                        return -3; //fail("archive_write_finish_entry()", archive_error_string(ext), 1);
+        }
+        archive_read_close(a);
+        archive_read_free(a);
+        
+        archive_write_close(ext);
+        archive_write_free(ext);
+
+        return 1;
+}
 
 
+//print error and exit:
+
+void err(std::string msg)
+{
+	printf("Error: %s\n",msg.c_str());
+	exit(0);
+}
 
 
 int main(int argc, char ** argv)
 {
-	int dbversion = 1;
+	int result;
 
-	std::string versions = getAsString("http://lensfun.sourceforge.net/db/versions.json");
+	//this is the lensfun databaseversion to be installed/updated
+	int dbversion = 1;
+	if (argc >=2) dbversion = atoi(argv[1]);
+
+	const std::string repositoryurl = "http://lensfun.sourceforge.net/db/";
+
+	//build the dir to store the lensfun database:
+	std::string dbdir = string_format("version_%d",dbversion);	
+
+	//get versions.json:
+	std::string versions = getAsString(string_format("%sversions.json",repositoryurl.c_str()));
 	std::cout << versions << "\n";
 
+	//parse timestamp and version numbers from downloaded versions.json:
 	std::string foo = removeall(versions,'[');
 	foo = removeall(foo,']');
 	std::vector<std::string> fields = split(foo,",");
@@ -227,12 +322,61 @@ int main(int argc, char ** argv)
 	bool versionavailable = false;
 	for (int i=0; i<fields.size(); i++) if (atoi(fields[i].c_str()) == dbversion) versionavailable = true;
 	
+	if (!versionavailable) 
+		err(string_format("Version %d not available.",dbversion));
+
+	//ToDo: check version_x/timestamp.txt against timestamp, if timestamp is <=, tell user the database is already at the current version
+	struct stat b;
+	bool isdirectory;
+	if (stat(dbdir.c_str(), &b) == 0) {
+		isdirectory = true;
+		std::ifstream tsfile;
+		tsfile.open(string_format("%s/timestamp.txt",dbdir.c_str()));
+		std::stringstream buffer;
+		buffer << tsfile.rdbuf();
+		int ts = atoi(buffer.str().c_str());
+		if (ts >= timestamp) 
+			err(string_format("local timestamp: %d >= server timestamp: %d; local database is the most current version.",ts,timestamp));
+	}
+	else isdirectory = false;
 	 
 	std::cout << "timestamp:" << timestamp << "  versionavailable:" << (versionavailable ? "true" : "false")  << "\n";
-	
-	std::string dburl = string_format("http://lensfun.sourceforge.net/db/version_%d.tar.bz2",dbversion);
 
+	//build the url and file to retrieve and store the database:
+	std::string dbfile = string_format("%s.tar.bz2",dbdir.c_str());
+	std::string dburl = string_format("%s%s",repositoryurl.c_str(),dbfile.c_str());
+
+	//get the database file to the current working directory:
 	if (!getAndSaveFile(dburl))
-		printf("Error: retrive %s failed.\n",dburl.c_str());
+		err(string_format("Retrive %s failed.",dburl.c_str()));
+
+	//store the working directory before cd'ing down into version_x/:
+	std::string prevdir = get_cwd();
+
+	//if the directory doesn't exist, create and cd into it
+	if (!isdirectory) {
+		mkdir(dbdir.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+		result = chdir(dbdir.c_str());
+	}
+	//if the directory exists, cd into it and delete all the files:
+	else {
+		result = chdir(dbdir.c_str());
+		std::vector<std::string> flist = filelist();
+		for (int i=0; i<flist.size(); i++)
+			remove(flist[i].c_str());
+	}
+	
+	//extract the database tar.bz2 into the version_x directory:
+	result = extract((std::string("../")+dbfile).c_str(), ARCHIVE_EXTRACT_TIME);
+
+	//ToDo: timestamp.txt
+	std::string timestampstring = string_format("%d",timestamp);
+	std::ofstream out("timestamp.txt");
+	out << timestampstring;
+	out.close();
+
+	//cd back to the original cwd, remove the version_x.tar.bz2:
+	result = chdir(prevdir.c_str());
+	remove(dbfile.c_str());
 }
 
